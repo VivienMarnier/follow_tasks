d57c7116fe0bec9e3f512024dc6603ba35d4ddc9
Vivien <vivienmarnier@gmail.com>
2017-03-06 12:30:13 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
+	//index : charge l'autoloader
 	require_once('modeles/Autoloader.php');
+	
+	//appel à la fonction register de l'autoload
 	Autoloader::register();
 	
 	error_reporting(E_ALL);
 	
+	//création d'un nouvel objet router 
 	$router = new Router();
+	
+	//appel de la fonction dispatch de notre router
 	$router->dispatch();
 ?>
 
